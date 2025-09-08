303360a81491971c51f7bf15ff0835b9d6225e3d
Thomas <th.neudoerfl@gmail.com>
2025-09-08 15:31:31 +0200

    points on sphere generation

--- LinesAndDotsTest.py
+++ LinesAndDotsTest.py
@@ -2,5 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import cos, sin
 
-fig = plt.figure(figsize=(5, 5))
+def generateSphere(radius=1, center=(0,0), numpoints=2**5):
+    points = []
+    angle = 2*np.pi/numpoints
+    for i in range(numpoints):
+        points.append((center[0]+radius*cos(i*angle), center[1]+radius*sin(i*angle)))
+    return points
+
+sphere = generateSphere()
+print(sphere)
+
+fig = plt.figure()
 plt.show()
