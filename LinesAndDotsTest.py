5ba2f55e7fedc9f314fc44f6a5ab222af04fa26a
Thomas <th.neudoerfl@gmail.com>
2025-09-08 16:13:42 +0200

    plot circle

--- LinesAndDotsTest.py
+++ LinesAndDotsTest.py
@@ -2,15 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import cos, sin
 
-def generateSphere(radius=1, center=(0,0), numpoints=2**5):
+def generateCircle(radius=1, center=(0,0), numpoints=2**5):
     points = []
     angle = 2*np.pi/numpoints
     for i in range(numpoints):
         points.append((center[0]+radius*cos(i*angle), center[1]+radius*sin(i*angle)))
     return points
 
-sphere = generateSphere()
-print(sphere)
+circle = generateCircle()
 
-fig = plt.figure()
+
+fig, ax = plt.subplots()
+for (x, y) in circle:
+    plt.plot(x, y, 'bo')
 plt.show()
