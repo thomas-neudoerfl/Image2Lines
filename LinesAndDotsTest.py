3429bd141019d991cb0501dd85f83780a2d64c68
Thomas <th.neudoerfl@gmail.com>
2025-09-16 17:46:30 +0200

    Read image and convert rgb to greyscale matrix

--- LinesAndDotsTest.py
+++ LinesAndDotsTest.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 from numpy import cos, sin
+
+img = mpimg.imread("BlackWhiteHeartTest.jpg")
+
+# Convert to grayscale
+gray = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
+
 
 def generateCircle(radius=1, center=(0,0), numpoints=2**5):
     points = []
