9d99ff2439c0c16c084bc27604ac3b8d3228a454
Thomas <th.neudoerfl@gmail.com>
2025-09-16 18:05:23 +0200

    Mapping real coordinate and image-Matrix indices

--- LinesAndDotsTest.py
+++ LinesAndDotsTest.py
@@ -6,8 +6,14 @@ from numpy import cos, sin
 img = mpimg.imread("BlackWhiteHeartTest.jpg")
 
 # Convert to grayscale
-gray = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
+image = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
+height = min(image.shape)
 
+def mapIndicesToCoords(i, j):
+    return ((2*j/height)-1, 1-(2*i/height))
+
+def mapCoordsToIndeces(x, y):
+    return (height*(y+1)//2, height*(x+1)//2)
 
 def generateCircle(radius=1, center=(0,0), numpoints=2**5):
     points = []
@@ -22,4 +28,9 @@ circle = generateCircle()
 fig, ax = plt.subplots()
 for (x, y) in circle:
     plt.plot(x, y, 'bo')
+for i in range(height):
+    for j in range(height):
+        if(image[i][j] < 100):
+            x, y = mapIndicesToCoords(i, j)
+            plt.plot(x, y, 'bo')
 plt.show()
