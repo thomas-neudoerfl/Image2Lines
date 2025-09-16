54e0613dc3ab712a2b219ff8b05c43115459bdaa
Thomas <th.neudoerfl@gmail.com>
2025-09-16 19:24:56 +0200

    Some error correction

--- LinesAndDotsTest.py
+++ LinesAndDotsTest.py
@@ -9,16 +9,22 @@ img = mpimg.imread("BlackWhiteHeartTest.jpg")
 image = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
 
 height = min(image.shape)
+numpoints = 2**5
 
-for i in range(height):
-    for j in range(height):
-        image[i][j] = 0.1*(256-image[i][j])
+def cleanImage():
+    for i in range(height):
+        for j in range(height):
+            x, y = mapIndicesToCoords(i, j)
+            if x**2 + y**2 > 1:
+                image[i][j] = 0
+            else:
+                image[i][j] = 0.1*(256-image[i][j])
 
 def mapIndicesToCoords(i, j):
     return ((2*j/height)-1, 1-(2*i/height))
 
 def mapCoordsToIndices(x, y):
-    return (height*(y+1)//2, height*(-x+1)//2)
+    return (int((height*(y+1))//2), int((height*(-x+1))//2))
 
 def generateCircle(radius=1, center=(0,0), numpoints=2**5):
     points = []
@@ -27,23 +33,20 @@ def generateCircle(radius=1, center=(0,0), numpoints=2**5):
         points.append((center[0]+radius*cos(i*angle), center[1]+radius*sin(i*angle)))
     return points
 
-numpoints = 2**5
-circle = generateCircle(numpoints=numpoints)
-
-fig, ax = plt.subplots()
 
 def sumOfLine(line):
     res = 0
-    for i in len(line):
-        res += image[line[i][0]][line[i][1]]
+    for pixel in line:
+        res += image[pixel[0]][pixel[1]]
+    return res
 
 def substractLine(line):
-    for i in len(line):
-        image[line[i][0]][line[i][1]] -= 1
+    for pixel in line:
+        image[pixel[0]][pixel[1]] -= 1
 
 def generateLines():
     res = []
-    while(sum(image) > 0):
+    while(np.sum(image) > 0):
         linePoints = ((0,0),(0,0))
         max = -np.infty
         linePixels = []
@@ -60,20 +63,33 @@ def generateLines():
                 else:
                     slope = (i1-i0)/(j1-j0)
                     for index in range(i0, i1+1):
-                        line.append((index, slope*index + i0))
+                        line.append((index, int(slope*index + i0)))
                 if sumOfLine(line) > max:
-                    linePoints = ((x0, y0), (x1, y1))
+                    linePoints = (([x0, x1]), ([y0, y1]))
                     linePixels = line
         substractLine(linePixels)
         res.append(linePoints)
+    return res
 
 
+cleanImage()
+
+circle = generateCircle(numpoints=numpoints)
+
+fig, ax = plt.subplots()
+
+lines = generateLines()
+
+for x, y in lines:
+    plt.plot(x, y, 'b-')
 
 for (x, y) in circle:
     plt.plot(x, y, 'bo')
+"""
 for i in range(height):
     for j in range(height):
         if(image[i][j] > 10):
             x, y = mapIndicesToCoords(i, j)
             plt.plot(x, y, 'bo')
+"""
 plt.show()
